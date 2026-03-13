0b2a0f9f4ed3ce9a7438b68f633d9513e5d677a2
josetomd <isset.josset@gmail.com>
2026-03-12 22:31:23 -0300

    Initial Commit

---
+++ CaloriesCounter/CaloriesCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesCounterApp.swift
+//  CaloriesCounter
+//
+//  Created by Josset Garcia on 12-03-26.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
