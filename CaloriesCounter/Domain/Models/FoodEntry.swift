20dec54a12e462357675dbc9f466b8da164ca1d1
josetomd <isset.josset@gmail.com>
2026-03-12 22:48:54 -0300

    create food model

---
+++ CaloriesCounter/Domain/Models/FoodEntry.swift
@@ -0,0 +1,32 @@
+//
+//  FoodEntry.swift
+//  CaloriesCounter
+//
+//  Created by Josset Garcia on 12-03-26.
+//
+
+import Foundation
+
+struct FoodEntry: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var calories: Int
+    var proteins: Double
+    var carbohydrates: Double
+    var fats: Double
+    var grams: Double
+    var amount: Int
+    var mealType: MealType
+
+    init(id: UUID = UUID(), name: String, calories: Int, proteins: Double, carbohydrates: Double, fats: Double, grams: Double, amount: Int, mealType: MealType) {
+        self.id = id
+        self.name = name
+        self.calories = calories
+        self.proteins = proteins
+        self.carbohydrates = carbohydrates
+        self.fats = fats
+        self.grams = grams
+        self.amount = amount
+        self.mealType = mealType
+    }
+}
---
+++ CaloriesCounter/Domain/Models/MealType.swift
@@ -0,0 +1,15 @@
+//
+//  MealType.swift
+//  CaloriesCounter
+//
+//  Created by Josset Garcia on 12-03-26.
+//
+
+import Foundation
+
+enum MealType: String {
+    case breakfast = "Breakfast"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+    case snack = "Snack"
+}
