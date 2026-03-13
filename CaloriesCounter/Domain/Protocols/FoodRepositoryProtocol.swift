af27913411b9a89afc79c1c4291f23e52ac0f261
josetomd <isset.josset@gmail.com>
2026-03-12 22:49:10 -0300

    create food protocol

---
+++ CaloriesCounter/Domain/Protocols/FoodRepositoryProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  FoodRepositoryProtocol.swift
+//  CaloriesCounter
+//
+//  Created by Josset Garcia on 12-03-26.
+//
+
+import Foundation
+
+protocol FoodRepositoryProtocol {
+    func save(_ food: FoodEntry) throws
+    func delete(id: UUID) throws
+    func fetchAll() throws -> [FoodEntry]
+    func fetch(for date: Date) throws -> [FoodEntry]
+    func update(_ food: FoodEntry) throws
+}
